8580d126cf448af423a6f73da5e34cf8c210e23d
mrx7355608 <mrx7355608@gmail.com>
2023-11-24 22:27:09 +0500

    writing parser tests

--- parser.js
+++ parser.js
@@ -1,14 +1,42 @@
 class Parser {
-    constructor(equation) {
-        this.partsOfQues = [];
-        this.regex = /(\(.+?\))|(!?[A-Za-z]+[.!]?[A-Za-z]*)/g;
-        this.eq = equation;
+    constructor() {
+        this.parsedExpression = [];
+        this.regex = /\+(?![^(]*\))/
     }
 
-    parse() {
-        const parts = this.eq.match(this.regex);
-        this.partsOfQues.push(...parts, this.eq);
-        return this.partsOfQues
+    validate(expression) {
+        const invalidInputsRegex = /[^!.()+ \[\]\{\}ABCD]/;
+
+        if (!expression) {
+            throw new Error("Expression is missing")
+        } 
+
+        expression = expression.trim(); // Remove spaces
+
+        if (expression.length < 1) {
+            throw new Error("Cannot parse empty expression")
+        }
+
+        if (invalidInputsRegex.test(expression)) {
+            throw new Error("Invalid expression")
+        }
+    }
+
+    parse(expression) {
+        // Validate the equation
+        this.validate(expression);
+
+        // If expression does have any brackets, return it as it is.
+        const bracketRegex = /[\(\)]/
+        if (!bracketRegex.test(expression)) {
+            return expression
+        }
+
+        // Otherwise, parse the expression
+        const parts = expression.split(this.regex);
+        this.parsedExpression.push(...parts, expression);
+
+        return this.parsedExpression
     }
 
     // TODO: add a clean function for removing
