422befb69f6a928cd44864bbc04ac8bf86eae665
mrx7355608 <mrx7355608@gmail.com>
2023-11-26 15:58:47 +0500

    create a basic homepage

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { __dirname } from "./utils/dirName.js";
+import main from "./solver/main.js";
 
 const app = express();
 const port = 3000;
@@ -10,6 +11,7 @@ const port = 3000;
 // TODO: add morgan, helmet and hpp middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use("/media", express.static(`${__dirname}/../public`))
 
 // Setting up views with ejs
 app.set("view engine", "ejs");
@@ -19,9 +21,15 @@ app.set("views", `${__dirname}/../views`);
 //         ROUTES
 // #######################
 
-app.get("/", (req, res) => {
-    return res.render("home", { title: 'HOME' });
+app.get("/", (_req, res) => {
+    return res.render("home", { title: 'Truth Table Generator' });
 })
+app.post("/", (req, res) => {
+    const { expression } = req.body;
+    main(expression);
+    return res.render("home", { title: 'Truth Table Generator' });
+})
+
 app.listen(port, () => {
     console.log("express app started on port", port)
 });
