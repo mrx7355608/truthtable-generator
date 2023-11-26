cca471d32d35626c4e78fad936d77cd31baba152
mrx7355608 <mrx7355608@gmail.com>
2023-11-26 15:16:45 +0500

    setting up ejs views

--- src/app.js
+++ src/app.js
@@ -1,2 +1,27 @@
-import main from "./solver/main.js";
+import express from "express";
+import { __dirname } from "./utils/dirName.js";
 
+const app = express();
+const port = 3000;
+
+// #######################
+//      APP CONFIG 
+// #######################
+// TODO: add morgan, helmet and hpp middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Setting up views with ejs
+app.set("view engine", "ejs");
+app.set("views", `${__dirname}/../views`);
+
+// #######################
+//         ROUTES
+// #######################
+
+app.get("/", (req, res) => {
+    return res.render("home", { title: 'HOME' });
+})
+app.listen(port, () => {
+    console.log("express app started on port", port)
+});
