8680b23e68a6db812be24bb999b872a106670738
mrx7355608 <mrx7355608@gmail.com>
2023-12-02 21:51:54 +0500

    wrapped everything in a main function

--- src/solver/main.js
+++ src/solver/main.js
@@ -3,17 +3,19 @@ import Solver from "./solver.js";
 import TruthTable from "./truthtable.js";
 import ValueReplacer from "./valueReplacer.js";
 
-const expression = "A.B + B.(B+C) + (A.D)"
-const parser = Parser(expression)
-const truthTable = TruthTable(expression)
+export const main = (expression) => {
+    const parser = Parser(expression)
+    const truthTable = TruthTable(expression)
 
-const parsedExpression = parser.parse();
-const values = truthTable.createBinaryCombinations();
+    const parsedExpression = parser.parse();
+    const values = truthTable.createBinaryCombinations();
 
-const valueReplacer = ValueReplacer(parsedExpression, values[0], values[1], values[2], values[3]);
-const totalCombinations = truthTable.getTotalCombintions();
-const solvables = valueReplacer.replaceValues(totalCombinations);
+    const valueReplacer = ValueReplacer(parsedExpression, values[0], values[1], values[2], values[3]);
+    const totalCombinations = truthTable.getTotalCombintions();
+    const solvables = valueReplacer.replaceValues(totalCombinations);
+    console.log({ solvables });
 
-const solver = Solver(parsedExpression, solvables)
-const answers = solver.solve(totalCombinations)
-console.log(answers)
+    const solver = Solver(parsedExpression, solvables)
+    const answers = solver.solve(totalCombinations)
+    return answers;
+}
