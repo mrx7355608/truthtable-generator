12bf7221b2d9d439e41095cc82fa0aa3cddfe05d
mrx7355608 <mrx7355608@gmail.com>
2023-12-05 02:17:25 +0500

    removed validator function (will add later)

--- src/solver/parser.js
+++ src/solver/parser.js
@@ -2,38 +2,16 @@ const Parser = (expression) => {
     const parsedExpression = [];
     const regex = /\(([^)]+)\)/g;
 
-    const validate = () => {
-        const invalidInputsRegex = /[^!.()+ \[\]\{\}ABCD]/;
-
-        if (!expression) {
-            throw new Error("Expression is missing")
-        } 
-
-        expression = expression.trim(); // Remove spaces
-
-        if (expression.length < 1) {
-            throw new Error("Cannot parse empty expression")
-        }
-
-        if (invalidInputsRegex.test(expression)) {
-            throw new Error("Invalid expression")
-        }
-    }
-
     const parse = () => {
-        // Validate the equation
-        validate();
-
         // If expression does have any brackets, return it as it is.
         const bracketRegex = /[\(\)]/
         if (!bracketRegex.test(expression)) {
-            return [ expression ]
+            return [expression]
         }
 
         // Otherwise, parse the expression
-        const parts = expression.match(regex);
-        parsedExpression.push(...parts, expression);
-
+        const subExpressions = expression.match(regex);
+        parsedExpression.push(...subExpressions, expression);
         return parsedExpression;
     }
 
